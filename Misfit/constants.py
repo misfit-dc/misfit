461776a8d150e5a70d8afdfc5a031ef2206c8ea3
Mannu Vilasara <117009138+MannuVilasara@users.noreply.github.com>
2023-06-26 23:13:19 +0530

    Delete Misfit directory

--- Misfit/constants.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-token = os.environ["token"]
-mongos = os.environ["mongos"]
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-spam = os.environ["SPAM_CHANNEL"]  
-modappch = os.environ["MOD_APPLICATION_CHANNEL"]
